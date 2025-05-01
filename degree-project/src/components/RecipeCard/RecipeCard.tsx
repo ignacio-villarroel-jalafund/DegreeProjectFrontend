4282afc0ab89a8033b0060132a92710d892840ed
Ignacio Villarroel <ignaciovillarroel50@gmail.com>
2025-05-01 19:56:14 -0400

    refactor: improve recipe card component

--- degree-project/src/components/RecipeCard/RecipeCard.tsx
+++ degree-project/src/components/RecipeCard/RecipeCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import type { Recipe } from '../../services/api';
+import { Link } from 'react-router-dom';
+import { Recipe } from '../../services/api';
 import styles from './RecipeCard.module.css';
 
 interface RecipeCardProps {
@@ -7,18 +8,15 @@ interface RecipeCardProps {
 }
 
 const RecipeCard: React.FC<RecipeCardProps> = ({ recipe }) => {
-  const imageUrl = recipe.img_src || '/icons/Burger_192.webp';
-
   return (
-    <div className={styles.recipeCard}>
-      <img src={imageUrl} alt={recipe.recipe_name} className={styles.recipeImage} loading="lazy" />
-      <div className={styles.recipeContent}>
-        <h3 className={styles.recipeTitle}>{recipe.recipe_name}</h3>
-        {/*recipe.total_time && <span>🕒 {recipe.total_time} min</span>*/}
-        {/* Enlace a una página de detalles (si la creas) */}
-        {/* <Link to={`/recipes/${recipe.id}`} className={styles.detailsLink}>Ver Detalles</Link> */}
+    <Link to={`/recipes/${recipe.id}`} className={styles.cardLink}>
+      <div className={styles.recipeCard}>
+        <img src={recipe.img_url} alt={recipe.recipe_name} className={styles.recipeImage} loading="lazy" />
+        <div className={styles.recipeContent}>
+          <h3 className={styles.recipeTitle}>{recipe.recipe_name}</h3>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
