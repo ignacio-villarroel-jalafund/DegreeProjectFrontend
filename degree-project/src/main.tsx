ab7185d494e495bf5bc0d48038d899ea540e7e45
Ignacio Villarroel <ignaciovillarroel50@gmail.com>
2025-04-30 22:02:43 -0400

    refactor: add components routes

--- degree-project/src/main.tsx
+++ degree-project/src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import { AuthProvider } from './contexts/AuthContext.tsx';
+import { SearchProvider } from './contexts/SearchContext.tsx';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <SearchProvider>
+            <App />
+        </SearchProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
