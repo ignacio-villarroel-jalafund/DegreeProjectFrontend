faaf8381f5302da96d98edd0db7a572e4a88634c
Ignacio Villarroel <ignaciovillarroel50@gmail.com>
2025-06-29 14:04:55 -0400

    refactor: modify search result to use reusable component

--- degree-project/src/pages/SearchResultsPage.tsx
+++ degree-project/src/pages/SearchResultsPage.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Link, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import { useSearch } from "../contexts/SearchContext";
-import { RecipeSearchResult } from "../services/api";
-import LoadingSpinner from "../components/UI/LoadingSpinner";
+import RecipeResultsGrid from "../components/Recipe/RecipeResultsGrid";
 import styles from "./HomePage.module.css";
 
 const SearchResultsPage: React.FC = () => {
@@ -21,56 +20,17 @@ const SearchResultsPage: React.FC = () => {
     }
   }, [query, handleSearch]);
 
-  const renderResults = (results: RecipeSearchResult[]) => (
-    <div className={styles.searchResultsList}>
-      {results.map((result) => (
-        <Link
-          key={result.url}
-          to={`/recipe/details?url=${encodeURIComponent(result.url)}`}
-          className={styles.searchResultItem}
-        >
-          <div className={styles.imageWrapper}>
-            <img
-              src={result.image_url || "/icons/Burger_192.webp"}
-              alt={`Imagen de ${result.title}`}
-              className={styles.recipeImage}
-              loading="lazy"
-            />
-          </div>
-          <div className={styles.itemContent}>
-            <h3>{result.title}</h3>
-            <span className={styles.detailsLink}>Ver Detalles →</span>
-          </div>
-        </Link>
-      ))}
-    </div>
-  );
-
   return (
     <div className={styles.homePageContainer}>
       <h2 className={styles.pageTitle}>
         {isLoadingSearch ? `Buscando "${query}"...` : pageTitle}
       </h2>
 
-      {isLoadingSearch && <LoadingSpinner />}
-
-      {!isLoadingSearch && searchError && (
-        <p className={styles.errorText}>{searchError}</p>
-      )}
-
-      {!isLoadingSearch &&
-        !searchError &&
-        searchResults &&
-        searchResults.length > 0 &&
-        renderResults(searchResults)}
-
-      {!isLoadingSearch &&
-        !searchError &&
-        searchResults?.length === 0 && (
-          <p className={styles.infoText}>
-            No se encontraron recetas para tu búsqueda.
-          </p>
-        )}
+      <RecipeResultsGrid
+        recipes={searchResults}
+        isLoading={isLoadingSearch}
+        error={searchError}
+      />
     </div>
   );
 };
